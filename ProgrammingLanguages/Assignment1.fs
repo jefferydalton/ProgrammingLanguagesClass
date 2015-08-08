2085e85f62c93929ce42524ec96abddf88d307b1
Jeff Dalton <jeff@simpleaspossible.xyz>
2015-08-07 21:59:23 -0400

    Initial

---
+++ ProgrammingLanguages/Assignment1.fs
@@ -0,0 +1,19 @@
+﻿module Assignment1
+
+
+let is_older first second =
+    let isValidDate x =
+        match x with
+        | (year, month, day) when year > 0 && month  >= 1 && month <= 12 && day > 1 && day <= 365 -> true
+        | _ -> false;
+
+    if isValidDate first = false then raise (System.Exception("invalid date"))
+    if isValidDate second = false then raise (System.Exception("invalid date"))
+     
+    if first >= second then true else false
+
+
+
+
+
+
---
+++ ProgrammingLanguages/TestFS.fsx
@@ -0,0 +1,10 @@
+﻿#r "Fsharp.Core"
+#r "FsCheck"
+
+open FsCheck
+
+let revRevIsOrig (xs:list<int>) = List.rev(List.rev xs) = x
+
+Check.Quick revRevIsOrig
+
+
